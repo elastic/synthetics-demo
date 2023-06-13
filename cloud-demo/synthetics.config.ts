2dda24fff4c08fe2cb6345f52dd848e561964e7b
Justin Kambic <jk@elastic.co>
2023-06-13 15:43:49 -0400

    Trying to test action.

--- cloud-demo/synthetics.config.ts
+++ cloud-demo/synthetics.config.ts
@@ -1,9 +1,9 @@
-import type { SyntheticsConfig } from '@elastic/synthetics';
+import type { SyntheticsConfig } from "@elastic/synthetics";
 
-export default env => {
+export default (env) => {
   const config: SyntheticsConfig = {
     params: {
-      url: 'https://elastic.github.io/synthetics-demo/',
+      url: "https://elastic.github.io/synthetics-demo/",
     },
     playwrightOptions: {
       ignoreHTTPSErrors: false,
@@ -13,22 +13,23 @@ export default env => {
      */
     monitor: {
       schedule: 10,
-      locations: ['germany','us_east'],
-      privateLocations: [''],
+      locations: ["germany", "us_east"],
+      privateLocations: [""],
     },
     /**
      * Project monitors settings
      */
     project: {
-      id: 'project-monitors-demo',
-      url: 'https://project-monitors-demo.kb.us-central1.gcp.cloud.es.io:9243/',
-      space: 'default',
+      id: "project-monitors-demo",
+      url: "https://project-monitors-demo.kb.us-central1.gcp.cloud.es.io:9243/",
+      // url: 'https://project-monitors-demo.kb.us-central1.gcp.cloud.es.io:9243/',
+      space: "default",
     },
   };
-  if (env !== 'development') {
+  if (env !== "development") {
     /**
      * Override configuration specific to environment
-     * Ex: config.params.url = ""
+     * Ex: config.params.url = ''
      */
   }
   return config;
